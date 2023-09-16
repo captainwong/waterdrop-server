9d46fb94b4deba9a84921624fd95f7e028bdfc45
i7-8700 <1281261856@qq.com>
2023-09-16 16:33:59 +0800

    uses table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule} from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST || 'localhost',
+      port: parseInt(process.env.MYSQL_PORT, 10) || 3306,
+      username: 'jack',
+      password: 'r7zuMTLmRJzKOrkb',
+      database: 'dropwater',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn({
+    comment: '用户ID',
+  })
+  id: number;
+
+  @Column({
+    comment: '用户名',
+    default: '',
+  })
+  @IsNotEmpty()
+  name: string;
+
+  @Column({
+    comment: '用户描述',
+    default: '',
+  })
+  desc: string;
+
+  @Column({
+    comment: '手机号码',
+    nullable: true,
+  })
+  tel: string;
+
+  @Column({
+    comment: '密码',
+    nullable: true,
+  })
+  password: string;
+
+  @Column({
+    comment: '账户信息',
+    nullable: true,
+  })
+  account: string;
+}
