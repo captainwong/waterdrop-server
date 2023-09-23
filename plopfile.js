1fd5baf7204f9140969ae1f724a3ba5da69d7871
i7-8700 <1281261856@qq.com>
2023-09-23 14:44:58 +0800

    plop

---
+++ plopfile.js
@@ -0,0 +1,123 @@
+const path = require('path');
+
+module.exports = (plop) => {
+  plop.setGenerator('module', {
+    description: 'Create a new module with service, resolver, and DTOs',
+    prompts: [
+      // yarn plop
+      {
+        type: 'input',
+        name: 'name',
+        message: '请输入模型的名字',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/{{kebabCase name}}.module.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/module.module.hbs',
+        ),
+      },
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/{{kebabCase name}}.service.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/module.service.hbs',
+        ),
+      },
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/{{kebabCase name}}.resolver.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/module.resolver.hbs',
+        ),
+      },
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/dto/create-{{kebabCase name}}.dto.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/dto/create-module.dto.hbs',
+        ),
+      },
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/dto/update-{{kebabCase name}}.dto.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/dto/update-module.dto.hbs',
+        ),
+      },
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/dto/{{kebabCase name}}-type.dto.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/dto/module-type.dto.hbs',
+        ),
+      },
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/dto/{{kebabCase name}}-result.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/dto/module-result.hbs',
+        ),
+      },
+      {
+        type: 'add',
+        path: path.join(
+          __dirname,
+          'src/modules/{{kebabCase name}}/entities/{{kebabCase name}}.entity.ts',
+        ),
+        templateFile: path.join(
+          __dirname,
+          'plopTemplates/module/entities/module.entity.hbs',
+        ),
+      },
+      {
+        type: 'modify',
+        path: './src/app.module.ts',
+        pattern: /import { Module } from '@nestjs\/common';/g,
+        template:
+          "import { Module } from '@nestjs/common';\nimport { {{pascalCase name}}Module } from './modules/{{kebabCase name}}/{{kebabCase name}}.module';",
+      },
+      {
+        type: 'modify',
+        path: './src/app.module.ts',
+        pattern: /imports: \[([\s\S]*?)\](?=\s*,\s*controllers)/g,
+        template: 'imports: [$1  {{pascalCase name}}Module,\n  ]',
+      },
+      {
+        type: 'append',
+        path: './src/common/const/code.ts',
+        templateFile: path.join(__dirname, 'plopTemplates/module/code.hbs'),
+      },
+    ],
+  });
+};
