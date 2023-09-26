84a04181725c4038bac1ee421a956be2a4d3c478
i7-8700 <1281261856@qq.com>
2023-09-26 21:58:27 +0800

    course input nullable

--- src/modules/course/dto/course-input.dto.ts
+++ src/modules/course/dto/course-input.dto.ts
@@ -4,33 +4,22 @@ import { TimeSlotsInput } from './common-input.dto';
 
 @InputType()
 export class CourseInputDto extends CommonInputDto {
-  @Field({
-    description: '课程名称',
-  })
+  @Field({ description: '课程名称', nullable: true })
   name: string;
 
-  @Field({
-    description: '课程描述',
-    nullable: true,
-  })
+  @Field({ description: '课程描述', nullable: true })
   desc: string;
 
-  @Field({
-    description: '适龄人群',
-  })
+  @Field({ description: '适龄人群', nullable: true })
   group: string;
 
-  @Field({
-    description: '适合基础',
-  })
+  @Field({ description: '适合基础', nullable: true })
   baseAbility: string;
 
-  @Field({
-    description: '学员人数上限',
-  })
+  @Field({ description: '学员人数上限', nullable: true })
   limit: number;
 
-  @Field({ description: '课程时长' })
+  @Field({ description: '课程时长', nullable: true })
   duration: number;
 
   @Field({ description: '预约信息', nullable: true })
