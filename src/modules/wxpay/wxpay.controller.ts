e1d1459e7ee5f840b23f75d5f19fdfed75999f65
i7-8700 <1281261856@qq.com>
2023-10-02 23:39:23 +0800

    save

--- src/modules/wxpay/wxpay.controller.ts
+++ src/modules/wxpay/wxpay.controller.ts
@@ -5,6 +5,7 @@ import { CurrentTokenId } from '@/common/decorators/current-token-id.decorator';
 import { AuthGuard } from '@/common/guards/auth.gard';
 import { STUDENT_NOT_EXISTS } from '@/common/const/code';
 import { CodeMsg } from '@/common/const/message';
+import axios from 'axios';
 
 @Controller('wxpay')
 export class WxpayController {
@@ -28,6 +29,27 @@ export class WxpayController {
       return;
     }
 
-    res.redirect(redirect);
+    const state = Buffer.from(
+      `${studentId}@${encodeURIComponent(redirect)}`,
+    ).toString('base64');
+
+    res.redirect(`
+      https://open.weixin.qq.com/connect/oauth2/authorize?appid=${process.env.WX_APPID}&redirect_uri=${process.env.WX_URL}/wxpay/loginCb&response_type=code&scope=snsapi_base&state=${state}#wechat_redirect
+    `);
+  }
+
+  @Get('loginCb')
+  async wxpayLoginCb(
+    @Query('code') code: string,
+    @Query('state') state: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    const [studentId, redirect] = Buffer.from(state, 'base64')
+      .toString()
+      .split('@');
+    const response = await axios.get(
+      `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${process.env.WX_APPID}&secret=${process.env.WX_SECRET}&code=${code}&grant_type=authorization_code`,
+    );
+    const { openid } = response.data;
   }
 }
